b4d9f885eff0e0559ad89bcb55d70cfd422bf038
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2019-03-27 15:43:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = '#'
 
 
-const developers = ["ID","ID"]
+const developers = ["515964196039557143"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
